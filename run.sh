1b0803f22244f0ca40c73ca3a6d3fc679c1bb617
ShiWenber <1210169842@qq.com>
2023-05-27 10:41:28 +0800

    feat(./run.sh): 为自动遍历参数的脚本更新cuda选择

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@ do
     for k in {1..16};
     do
         echo $i, 0.0$j, $k >> temp.log;
-        python main.py --dataname MUTAG --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/MUTAG.log;
-        python main.py --dataname IMDB-BINARY --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/IMDB-BINARY.log;
-        python main.py --dataname IMDB-MULTI --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/IMDB-MULTI.log;
-        python main.py --dataname PROTEINS --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/PROTEINS.log;
-        # python main.py --dataname REDDIT-BINARY --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/REDDIT-BINARY.log;
-        python main.py --dataname NCI1 --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX  > nohup_logs/${time_str}/NCI1.log;
-        # python main.py --dataname REDDIT-MULTI-5K --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/REDDIT-MULTI-5K.log;
-        python main.py --dataname COLLAB --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/COLLAB.log;
-        # python main.py --dataname DD --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX  > nohup_logs/${time_str}/DD.log;
-        python main.py --dataname ENZYMES --depth $i --rate 0.$j  --ring_width $k  --cuda CUDA_IDX >> nohup_logs/${time_str}/ENZYMES.log;
+        python main.py --dataname MUTAG --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/MUTAG.log;
+        python main.py --dataname IMDB-BINARY --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/IMDB-BINARY.log;
+        python main.py --dataname IMDB-MULTI --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/IMDB-MULTI.log;
+        python main.py --dataname PROTEINS --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/PROTEINS.log;
+        # python main.py --dataname REDDIT-BINARY --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/REDDIT-BINARY.log;
+        python main.py --dataname NCI1 --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX}  > nohup_logs/${time_str}/NCI1.log;
+        # python main.py --dataname REDDIT-MULTI-5K --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/REDDIT-MULTI-5K.log;
+        python main.py --dataname COLLAB --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/COLLAB.log;
+        # python main.py --dataname DD --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX}  > nohup_logs/${time_str}/DD.log;
+        python main.py --dataname ENZYMES --depth $i --rate 0.$j  --ring_width $k  --cuda ${CUDA_IDX} >> nohup_logs/${time_str}/ENZYMES.log;
     done
     # for k in {2..7};
     # do
